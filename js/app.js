ce286814bd56e44c111504f8ef88ee87176e7150
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-10 00:33:47 +0600

    [FIX]-total-price

--- js/app.js
+++ js/app.js
@@ -47,6 +47,7 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal();
 };
 
 const showProductDetails = (product_id) => {
@@ -75,7 +76,6 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-//   document.getElementById(id).innerText = Math.round(total);
   document.getElementById(id).innerText = total.toFixed(2);
 };
 
